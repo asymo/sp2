9a491f2f0a9f2053d5410fdc0c052f2d2f497783
Chris <cjsymons86@gmail.com>
2016-10-10 19:55:02 +0100

    updated work

--- Lab1/src/PercentagePrinter.java
+++ Lab1/src/PercentagePrinter.java
@@ -2,6 +2,8 @@
 public class PercentagePrinter {
 	public static void main(String[] args) {
 		double probability = 8.70;
+		//int percentage = (int) (100 * probability);
+		// without rounding answer is 896
 		int percentage = (int)Math.round(100 * probability);
 		System.out.println(percentage);
 	}
